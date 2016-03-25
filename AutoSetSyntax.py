c804afeab9ae01bcc532bea82b26f03121e5511a
Jack Cherng <jfcherng@gmail.com>
2016-03-25 18:14:03 +0800

    nits
    resolve a flake8 warning

--- AutoSetSyntax.py
+++ AutoSetSyntax.py
@@ -6,7 +6,7 @@ import sublime_plugin
 import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
-from SyntaxMappings import *
+from SyntaxMappings import SyntaxMappings
 
 
 PLUGIN_NAME = 'AutoSetSyntax'
