1b250132795aca5e7af0f3cdb8c0954cb8eda959
Jack Cherng <jfcherng@gmail.com>
2021-07-29 09:05:59 +0800

    style: make CI happy
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/constant.py
+++ plugin/constant.py
@@ -33,4 +33,4 @@ PLUGIN_CUSTOM_MODULE_PATHS = {
 VIEW_RUN_ID_SETTINGS_KEY = f"{PLUGIN_NAME}/run_id"
 VIEW_IS_TRANSIENT_SETTINGS_KEY = f"{PLUGIN_NAME}/is_transient"
 
-RE_VIM_SYNTAX_LINE = re.compile(rf"\bsyntax=(?P<syntax>[^\s]+)")
+RE_VIM_SYNTAX_LINE = re.compile(r"\bsyntax=(?P<syntax>[^\s]+)")
