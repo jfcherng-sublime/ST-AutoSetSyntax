17ecdad1258300d92049eff9b65642c4686a618c
Jack Cherng <jfcherng@gmail.com>
2021-08-19 10:49:07 +0800

    chore: bump 2.1.11-dev
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/constant.py
+++ plugin/constant.py
@@ -5,7 +5,7 @@ import sys
 
 ################################################################################
 
-VERSION_INFO = (2, 1, 10, "stable")
+VERSION_INFO = (2, 1, 11, "dev")
 VERSION = ".".join(map(str, VERSION_INFO[:3]))
 if next(filter(None, VERSION_INFO[3:4]), None):
     VERSION += f"-{VERSION_INFO[3]}"
