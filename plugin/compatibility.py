3ff98ca8af7df5d32da22eaa14f870f78eaac4e0
Jack Cherng <jfcherng@gmail.com>
2021-08-19 10:47:37 +0800

    fix: AttributeError: type object 'View' has no attribute 'clear_undo_stack'
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/compatibility.py
+++ plugin/compatibility.py
@@ -2,7 +2,7 @@ from typing import Callable, Optional
 import sublime
 
 # available as of ST 4114
-_view_clear_undo_stack: Optional[Callable[[sublime.View], None]] = getattr(sublime.View, "clear_undo_stack")
+_view_clear_undo_stack: Optional[Callable[[sublime.View], None]] = getattr(sublime.View, "clear_undo_stack", None)
 
 
 def view_clear_undo_stack(view: sublime.View) -> None:
