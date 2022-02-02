ea0abc50c1669d166659c6a9cb3ca994da2c96b9
Jack Cherng <jfcherng@gmail.com>
2022-02-03 01:30:54 +0800

    refactor: make checking guesslang server started more clearly
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/guesslang/server.py
+++ plugin/guesslang/server.py
@@ -3,121 +3,109 @@ from ..constant import PLUGIN_STORAGE_DIR
 from ..helper import expand_variables
 from ..logger import Logger
 from ..settings import get_merged_plugin_setting
-from typing import Dict, List, Optional, Sequence, Union
+from pathlib import Path
+from typing import Dict, Optional, Set, Sequence, Union
 import os
 import shutil
 import socket
 import sublime
 import subprocess
 
-# background server process(es)
-_subprocesses: List[subprocess.Popen] = []
-
 
 class GuesslangServer:
     server_dir = PLUGIN_STORAGE_DIR / "guesslang-server"
     server_file = PLUGIN_STORAGE_DIR / "guesslang-server/websocket.js"
 
+    # background server process(es)
+    _subprocesses: Set[subprocess.Popen] = set()
+
     @classmethod
     def start(cls, host: str, port: int) -> bool:
         """Starts the guesslang server and return whether it starts."""
-        node_path = parse_node_path()
-        if not is_executable(node_path):
-            sublime.error_message(
-                f'[{PLUGIN_NAME}] Node.js binary not found or not executable. Exepected path: "{node_path}"'
-            )
+        if not is_executable(node_path := parse_node_path()):
+            sublime.error_message(f'[{PLUGIN_NAME}] Node.js binary not found or not executable: "{node_path}"')
             return False
 
         if is_port_in_use(port):
             Logger.log(sublime.active_window(), f"⚠ Port {port} is in use.")
-        else:
-            try:
-                process = start_server_process(
-                    (node_path, str(cls.server_file)),
-                    cwd=str(cls.server_dir),
-                    extra_env={
-                        "NODE_SKIP_PLATFORM_CHECK": "1",
-                        "HOST": host,
-                        "PORT": str(port),
-                    },
-                )
-                _, stderr = process.communicate(timeout=1)
-            except subprocess.TimeoutExpired:
-                return True
-            except Exception as e:
-                sublime.error_message(f"[{PLUGIN_NAME}] Fail starting guesslang server.\n\n{e}")
-                return False
-
-            sublime.error_message(f"[{PLUGIN_NAME}] Fail starting guesslang server.\n\n{stderr}")
-
-            try:
-                _subprocesses.remove(process)
-            except ValueError:
-                pass
             return False
 
-        return True
+        try:
+            process = cls._start_process(
+                (node_path, cls.server_file),
+                cwd=cls.server_dir,
+                extra_env={
+                    "NODE_SKIP_PLATFORM_CHECK": "1",
+                    "HOST": host,
+                    "PORT": str(port),
+                },
+            )
+        except Exception as e:
+            sublime.error_message(f"[{PLUGIN_NAME}] Fail starting guesslang server because {e}")
+            return False
+
+        if process.stdout and process.stdout.read(2) == "OK":
+            cls._subprocesses.add(process)
+            return True
+
+        sublime.error_message(f"[{PLUGIN_NAME}] Fail starting guesslang server.")
+        return False
 
     @classmethod
     def stop(cls) -> None:
-        for p in _subprocesses:
+        for p in cls._subprocesses:
             try:
                 p.kill()
             except Exception:
                 pass
-        for p in _subprocesses:
+        for p in cls._subprocesses:
             try:
                 p.wait()
             except Exception:
                 pass
-        _subprocesses.clear()
+        cls._subprocesses.clear()
 
     @classmethod
     def is_running(cls) -> bool:
-        return len(_subprocesses) > 0
+        return len(cls._subprocesses) > 0
+
+    @staticmethod
+    def _start_process(
+        cmd: Union[str, Path, Sequence[Union[str, Path]]],
+        extra_env: Optional[Dict[str, str]] = None,
+        **kwargs,
+    ) -> subprocess.Popen:
+        if os.name == "nt":
+            # do not create a window for the process
+            startupinfo = subprocess.STARTUPINFO()  # type: ignore
+            startupinfo.dwFlags |= subprocess.SW_HIDE | subprocess.STARTF_USESHOWWINDOW  # type: ignore
+        else:
+            startupinfo = None  # type: ignore
+
+        env = os.environ.copy()
+        env.update(extra_env or {})
+
+        return subprocess.Popen(
+            cmd,
+            startupinfo=startupinfo,
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            encoding="utf-8",
+            env=env,
+            **kwargs,
+        )
 
 
 def parse_node_path() -> str:
     return expand_variables(get_merged_plugin_setting("guesslang.node_bin"))
 
 
-def is_executable(path: str) -> bool:
+def is_executable(path: Union[str, Path]) -> bool:
     return bool((os.path.isfile(path) and os.access(path, os.X_OK)) or shutil.which(path))
 
 
 def is_port_in_use(port: int) -> bool:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(("localhost", port)) == 0
-
-
-def start_server_process(
-    cmd: Union[str, Sequence[str]],
-    cwd: str = "",
-    shell: bool = False,
-    extra_env: Optional[Dict[str, str]] = None,
-) -> subprocess.Popen:
-    if os.name == "nt":
-        # do not create a window for the process
-        startupinfo = subprocess.STARTUPINFO()  # type: ignore
-        startupinfo.dwFlags |= subprocess.SW_HIDE | subprocess.STARTF_USESHOWWINDOW  # type: ignore
-    else:
-        startupinfo = None  # type: ignore
-
-    env = os.environ.copy()
-    env.update(extra_env or {})
-
-    process = subprocess.Popen(
-        cmd,
-        cwd=cwd,
-        startupinfo=startupinfo,
-        stdin=subprocess.DEVNULL,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.PIPE,
-        text=True,
-        encoding="utf-8",
-        shell=shell,
-        env=env,
-    )
-    _subprocesses.append(process)
-
-    return process
