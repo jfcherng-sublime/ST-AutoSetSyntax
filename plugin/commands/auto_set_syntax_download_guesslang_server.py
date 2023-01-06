64bcb5594b0a336a25a4d9e4bc0dfd66098d3346
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2023-01-05 18:47:16 -0600

    fix: CVE-2007-4559 Patch (#15)

--- plugin/commands/auto_set_syntax_download_guesslang_server.py
+++ plugin/commands/auto_set_syntax_download_guesslang_server.py
@@ -111,12 +111,53 @@ def decompress_file(tarball: PathLike, dst_dir: Optional[PathLike] = None) -> bo
     try:
         if filename.endswith(".tar.gz"):
             with tarfile.open(tarball, "r:gz") as f_1:
-                f_1.extractall(dst_dir)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(f_1, dst_dir)
             return True
 
         if filename.endswith(".tar"):
             with tarfile.open(tarball, "r:") as f_2:
-                f_2.extractall(dst_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(f_2, dst_dir)
             return True
 
         if filename.endswith(".zip"):
