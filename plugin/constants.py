d94c5c8df865e27f46ff6a61c4ac62958f912c95
Jack Cherng <jfcherng@gmail.com>
2025-10-28 17:48:46 +0800

    fix: py313 compatibility
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin/constants.py
+++ plugin/constants.py
@@ -10,6 +10,10 @@ assert __package__
 
 ################################################################################
 
+PY_SHORT_VERSION = "".join(map(str, sys.version_info[:2]))  # like "38" or "313"
+
+################################################################################
+
 VERSION_INFO = (4, 3, 7)
 VERSION = ".".join(map(str, VERSION_INFO))
 
@@ -34,7 +38,7 @@ PLUGIN_CUSTOM_MODULE_PATHS = {
     "match": PLUGIN_CUSTOM_DIR / "matches",
 }
 
-PLUGIN_PY_LIBS_DIR_NAME = f"libs-py38@{ST_PLATFORM_ARCH}"
+PLUGIN_PY_LIBS_DIR_NAME = f"libs-py{PY_SHORT_VERSION}@{ST_PLATFORM_ARCH}"
 PLUGIN_PY_LIBS_DIR = PLUGIN_STORAGE_DIR / PLUGIN_PY_LIBS_DIR_NAME
 PLUGIN_PY_LIBS_ZIP_NAME = f"{PLUGIN_PY_LIBS_DIR_NAME}.tar.xz"
 PLUGIN_PY_LIBS_URL = "https://github.com/{repo}/raw/{ref}/{file}".format(
